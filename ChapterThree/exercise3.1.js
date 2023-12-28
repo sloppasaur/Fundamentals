fd3c4747ef54c31aab73aab027a65a2508ddba27
Nathaniel Barrera <nathaniel.barrera39@gmail.com>
2023-12-27 21:03:06 -0600

    Added and complete exercise 3.1

---
+++ ChapterThree/exercise3.1.js
@@ -0,0 +1,15 @@
+// Nathaniel Barrera
+// The goal of the exercise is to practice using arrays:
+// 1. Create an Array with "Milk", "Bread", "Apples"
+// 2. Check the length in the console
+// 3. Update "Bread" to "Bananas"
+// 4. Output your entire list to the console
+
+// Create a list with said items, then display the array and the number of items
+groceryList = ["Milk", "Bread", "Apples"];
+console.log(`Here is the list: ${groceryList}`);
+console.log(`The length of the list: ${groceryList.length} items`);
+
+// Change "Bread" to "Bananas"
+groceryList[1] = "Bananas";
+console.log(`Here is the new list: ${groceryList}`);
